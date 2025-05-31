3c714a0786012cd3c350cd80486f8d4c488ca0ed
Homestate-dev <homestate.dev@gmail.com>
2025-05-31 10:21:48 -0300

    Primero pasos para crear un edificio
    Ya se puede crear un edificio con nombre y direccion

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('admin-login/', views.admin_login, name='admin-login'),
     path('admin/', views.admin, name='admin'),
+    path('building/create/', views.building_create, name='building-create'),
 ]
--- core/views.py
+++ core/views.py
@@ -17,3 +17,19 @@ def admin(request):
     #aqui quiero mostrar el email del usuario que se loguea
     print(f"User email: {user_email}") 
     return render(request, 'core/admin/dashboard.html', context)
+
+def building_create(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre_edificio')
+        direccion = request.POST.get('direccion')
+
+        # Imprimimos en la consola de Django que recibimos los valores:
+        print(f"Datos recibidos para crear edificio → Nombre: {nombre}, Dirección: {direccion}")
+
+        contexto = {
+            'mensaje_exito': "¡Se recibieron correctamente los datos!",
+        }
+        return render(request, 'core/building/create.html', contexto)
+
+    # Si es GET, simplemente renderizamos el formulario sin contexto especial.
+    return render(request, 'core/building/create.html')
